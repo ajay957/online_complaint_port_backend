dcbe397bea7a3c1fa11be53dab5fd7e2d0c90105
Ajay957 <ajay74.aa82@gmail.com>
2022-12-09 14:26:32 +0530

    first commit

---
+++ src/main/java/com/example/OnlineComplaintReportingPort/OnlineComplaintReportingPortApplication.java
@@ -0,0 +1,13 @@
+package com.example.OnlineComplaintReportingPort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineComplaintReportingPortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineComplaintReportingPortApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/OnlineComplaintReportingPort/OnlineComplaintReportingPortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OnlineComplaintReportingPort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineComplaintReportingPortApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
